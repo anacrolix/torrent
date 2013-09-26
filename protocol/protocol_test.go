0866ce3579d29692c2e9e748e15fe6da6b381732
Matt Joiner <anacrolix@gmail.com>
2013-09-26 13:43:46 +1000

    Add some basic protocol definitions, this will be improved in the future

---
+++ protocol/protocol_test.go
@@ -0,0 +1,12 @@
+package protocol
+
+import (
+	"testing"
+)
+
+func TestConstants(t *testing.T) {
+	// check that iota works as expected in the const block
+	if NotInterested != 3 {
+		t.FailNow()
+	}
+}

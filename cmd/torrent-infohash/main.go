7928b763d98a08f076f829320bfef42c687be77d
Matt Joiner <anacrolix@gmail.com>
2014-07-10 00:15:03 +1000

    torrent-infohash: utility to print the info hash of a torrent file

---
+++ cmd/torrent-infohash/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/anacrolix/libtorgo/metainfo"
+	"log"
+)
+
+func main() {
+	flag.Parse()
+	for _, arg := range flag.Args() {
+		mi, err := metainfo.LoadFromFile(arg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%x: %s\n", mi.InfoHash, arg)
+	}
+}

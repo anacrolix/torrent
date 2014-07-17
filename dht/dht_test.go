9c575b0454594bd9cdac5ed8ae8349b9a011a492
Matt Joiner <anacrolix@gmail.com>
2014-07-17 16:05:21 +1000

    dht_test: Add commented out test I haven't resolved yet

--- dht/dht_test.go
+++ dht/dht_test.go
@@ -89,3 +89,23 @@ func TestClosestNodes(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+// func TestUnmarshalGetPeersResponse(t *testing.T) {
+// 	var gpr getPeersResponse
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	err = dec.Decode(map[string]interface{}{
+// 		"values": []string{"\x01\x02\x03\x04\x05\x06", "\x07\x08\x09\x0a\x0b\x0c"},
+// 		"nodes":  "\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d",
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	if len(gpr.Values) != 2 {
+// 		t.FailNow()
+// 	}
+// 	if len(gpr.Nodes) != 2 {
+// 		t.FailNow()
+// 	}
+// }

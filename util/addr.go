caa389784adf6b18b1a685eca6cbcfc0691a2f22
Matt Joiner <anacrolix@gmail.com>
2014-12-26 17:16:20 +1100

    util.Addr* funcs: Add optimizations for *net.UDPAddr

--- util/addr.go
+++ util/addr.go
@@ -7,21 +7,31 @@ import (
 
 // Extracts the port as an integer from an address string.
 func AddrPort(addr net.Addr) int {
-	_, port, err := net.SplitHostPort(addr.String())
-	if err != nil {
-		panic(err)
+	switch raw := addr.(type) {
+	case *net.UDPAddr:
+		return raw.Port
+	default:
+		_, port, err := net.SplitHostPort(addr.String())
+		if err != nil {
+			panic(err)
+		}
+		i64, err := strconv.ParseInt(port, 0, 0)
+		if err != nil {
+			panic(err)
+		}
+		return int(i64)
 	}
-	i64, err := strconv.ParseInt(port, 0, 0)
-	if err != nil {
-		panic(err)
-	}
-	return int(i64)
 }
 
 func AddrIP(addr net.Addr) net.IP {
-	host, _, err := net.SplitHostPort(addr.String())
-	if err != nil {
-		panic(err)
+	switch raw := addr.(type) {
+	case *net.UDPAddr:
+		return raw.IP
+	default:
+		host, _, err := net.SplitHostPort(addr.String())
+		if err != nil {
+			panic(err)
+		}
+		return net.ParseIP(host)
 	}
-	return net.ParseIP(host)
 }

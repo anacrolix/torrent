d9ec43abb8481280f18a702efc59ee19efa0ea5d
Dmitry Borzov <tihoutrom@gmail.com>
2015-12-07 16:56:21 +0300

    Move from log.Fatal to plain Printfs for cli error messages

--- cmd/dht-ping/main.go
+++ cmd/dht-ping/main.go
@@ -1,4 +1,5 @@
-// Pings DHT nodes with the given network addresses.
+// dht-ping is a cli tool that pings DHT nodes with the given UDP network
+// addresses and returns NodeID if sucessful
 package main
 
 import (
@@ -22,20 +23,28 @@ func main() {
 	err := CommandLine.Parse(os.Args[1:])
 	pingStrAddrs := CommandLine.Args()
 
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	if len(pingStrAddrs) == 0 {
 		os.Stderr.WriteString("Error: no target UDP node addresses specified\n")
 		usageMessageAndQuit()
 	}
+
+	err = sendPings(pingStrAddrs)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(0)
+	}
+}
+
+// PingAddresses attempts to send the ping KRPC messages to the UDP addresses
+func sendPings(targets []string) error {
 	s, err := dht.NewServer(nil)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("cannot access UDP server for the dht node: %s", err)
 	}
-	log.Printf("dht server on %s", s.Addr())
 	pingResponsesChan := make(chan pingResponse)
 	timeoutChan := make(chan struct{})
 	go func() {
-		for i, netloc := range pingStrAddrs {
+		for i, netloc := range targets {
 			if i != 0 {
 				time.Sleep(1 * time.Millisecond)
 			}
@@ -66,7 +75,7 @@ func main() {
 	}()
 	responses := 0
 pingResponsesLoop:
-	for _ = range pingStrAddrs {
+	for _ = range targets {
 		select {
 		case resp := <-pingResponsesChan:
 			if !resp.msgOk {
@@ -77,6 +86,8 @@ pingResponsesLoop:
 		case <-timeoutChan:
 			break pingResponsesLoop
 		}
+		fmt.Printf("%d/%d responses (%f%%)\n", responses, len(targets), 100*float64(responses)/float64(len(targets)))
+
 	}
-	fmt.Printf("%d/%d responses (%f%%)\n", responses, len(pingStrAddrs), 100*float64(responses)/float64(len(pingStrAddrs)))
+	return nil
 }

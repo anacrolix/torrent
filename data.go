ca3529bd9a6bac1a87cb47ba185c1490ab5e2836
Matt Joiner <anacrolix@gmail.com>
2016-03-26 18:27:28 +1100

    Improve doc comments on Data

--- data.go
+++ data.go
@@ -4,11 +4,15 @@ import "io"
 
 // Represents data storage for a Torrent.
 type Data interface {
+	// Should return io.EOF only at end of torrent. Short reads due to missing
+	// data should return io.ErrUnexpectedEOF.
 	io.ReaderAt
 	io.WriterAt
 	// Bro, do you even io.Closer?
 	Close()
-	// We believe the piece data will pass a hash check.
+	// Called when the client believes the piece data will pass a hash check.
+	// The storage can move or mark the piece data as read-only as it sees
+	// fit.
 	PieceCompleted(index int) error
 	// Returns true if the piece is complete.
 	PieceComplete(index int) bool

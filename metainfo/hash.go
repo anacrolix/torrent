7044161faad877dd8a764b485b856ddb2a450167
Matt Joiner <anacrolix@gmail.com>
2016-05-02 11:21:03 +1000

    Add metainfo.Hash.FromHexString

--- metainfo/hash.go
+++ metainfo/hash.go
@@ -1,6 +1,9 @@
 package metainfo
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+)
 
 // 20-byte SHA1 hash used for info and pieces.
 type Hash [20]byte
@@ -15,4 +18,19 @@ func (h *Hash) AsString() string {
 
 func (h Hash) HexString() string {
 	return fmt.Sprintf("%x", h[:])
+}
+
+func (h *Hash) FromHexString(s string) (err error) {
+	if len(s) != 40 {
+		err = fmt.Errorf("hash hex string has bad length: %d", len(s))
+		return
+	}
+	n, err := hex.Decode(h[:], []byte(s))
+	if err != nil {
+		return
+	}
+	if n != 20 {
+		panic(n)
+	}
+	return
 }

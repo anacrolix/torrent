ad5d5bc35038058d218d85a391deb50cbb8d5758
Alexey Kuznetsov <axet@me.com>
2016-06-15 18:25:48 +0300

    -a option (#86)
    * add -a option
    
    * name

--- cmd/torrent-create/main.go
+++ cmd/torrent-create/main.go
@@ -20,12 +20,16 @@ var (
 func main() {
 	log.SetFlags(log.Flags() | log.Lshortfile)
 	var args struct {
+		AnnonceList []string `name:"a" help:"add annonce server"`
 		tagflag.StartPos
-		Root string
+		Root string `help:"Creates a torrent metainfo for the file system rooted at ROOT, and outputs it to stdout."`
 	}
-	tagflag.Parse(&args, tagflag.Description("Creates a torrent metainfo for the file system rooted at ROOT, and outputs it to stdout."))
+	tagflag.Parse(&args)
 	mi := metainfo.MetaInfo{
 		AnnounceList: builtinAnnounceList,
+	}
+	for _, a := range args.AnnonceList {
+		mi.AnnounceList = append(mi.AnnounceList, []string{a})
 	}
 	mi.SetDefaults()
 	err := mi.Info.BuildFromFilePath(args.Root)

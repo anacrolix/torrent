8d0bd910a719965a0962f2a1498b01db89ae3781
Matt Joiner <anacrolix@gmail.com>
2016-07-11 13:52:51 +1000

    List implemented BEPs
    Fix issue #93.

--- doc.go
+++ doc.go
@@ -13,5 +13,22 @@ BitTorrent features implemented include:
  * IP Blocklists
  * Some IPv6
  * HTTP and UDP tracker clients
+ * BEPs:
+  -  3: Basic BitTorrent protocol
+  -  5: DHT
+  -  6: Fast Extension (have all/none only)
+  -  7: IPv6 Tracker Extension
+  -  9: ut_metadata
+  - 10: Extension protocol
+  - 11: PEX
+  - 12: Multitracker metadata extension
+  - 15: UDP Tracker Protocol
+  - 20: Peer ID convention ("-GTnnnn-")
+  - 23: Tracker Returns Compact Peer Lists
+  - 27: Private torrents
+  - 29: uTorrent transport protocol
+  - 41: UDP Tracker Protocol Extensions
+  - 42: DHT Security extension
+  - 43: Read-only DHT Nodes
 */
 package torrent

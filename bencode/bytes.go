ebdab2d9def0954bf9d6f9cbb43e4602ee9b5153
Matt Joiner <anacrolix@gmail.com>
2016-08-26 14:51:09 +1000

    Add bencode.Bytes

---
+++ bencode/bytes.go
@@ -0,0 +1,17 @@
+package bencode
+
+type Bytes []byte
+
+var (
+	_ Unmarshaler = &Bytes{}
+	_ Marshaler   = &Bytes{}
+)
+
+func (me *Bytes) UnmarshalBencode(b []byte) error {
+	*me = append([]byte(nil), b...)
+	return nil
+}
+
+func (me *Bytes) MarshalBencode() ([]byte, error) {
+	return *me, nil
+}

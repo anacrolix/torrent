1f2f037209405c7be79755d7f02f33d2024d0d18
Matt Joiner <anacrolix@gmail.com>
2016-11-30 18:01:39 +1100

    Fix logging of trust slice

--- torrent.go
+++ torrent.go
@@ -1446,12 +1446,12 @@ func (t *Torrent) pieceHashed(piece int, correct bool) {
 			c.badPiecesDirtied++
 		}
 		slices.Sort(touchers, connLessTrusted)
-		log.Printf("dropping first corresponding conn from trust: %s", func() (ret []int) {
+		log.Printf("dropping first corresponding conn from trust: %v", func() (ret []int) {
 			for _, c := range touchers {
 				ret = append(ret, c.netGoodPiecesDirtied())
 			}
 			return
-		})
+		}())
 		c := touchers[0]
 		t.cl.banPeerIP(missinggo.AddrIP(c.remoteAddr()))
 		c.Drop()

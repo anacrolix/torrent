afb83e792b26702de48b817ca106d03abbe8de58
Matt Joiner <anacrolix@gmail.com>
2017-08-27 14:03:10 +1000

    fs/test.sh: Update torrentfs arguments and wait for info to be fetched so file isn't found

--- fs/test.sh
+++ fs/test.sh
@@ -1,7 +1,8 @@
 mkdir mnt torrents
-GOPPROF=http godo github.com/anacrolix/torrent/cmd/torrentfs -mountDir mnt -torrentPath torrents &
+GOPPROF=http godo github.com/anacrolix/torrent/cmd/torrentfs -mountDir=mnt -metainfoDir=torrents &
 cd torrents
 wget http://releases.ubuntu.com/14.04.2/ubuntu-14.04.2-desktop-amd64.iso.torrent
 cd ..
-ls mnt
-pv mnt/ubuntu-14.04.2-desktop-amd64.iso | md5sum
+file=ubuntu-14.04.2-desktop-amd64.iso
+while [ ! -e "mnt/$file" ]; do sleep 1; done
+pv "mnt/$file" | md5sum

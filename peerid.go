d5f8440e7e5b30943d23a9adf0039d6aec1436b0
Matt Joiner <anacrolix@gmail.com>
2018-01-25 13:14:37 +1100

    Add some helpful comments to type PeerID

--- peerid.go
+++ peerid.go
@@ -4,8 +4,11 @@ import (
 	"encoding/hex"
 )
 
+// Peer client ID.
 type PeerID [20]byte
 
+// Pretty prints the ID as hex, except parts that adher to the Peer ID
+// Conventions of BEP 20.
 func (me PeerID) String() string {
 	if me[0] == '-' && me[7] == '-' {
 		return string(me[:8]) + hex.EncodeToString(me[8:])

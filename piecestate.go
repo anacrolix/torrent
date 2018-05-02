52896818db538b670887e28cf5eaf194514a1369
Matt Joiner <anacrolix@gmail.com>
2018-05-02 19:23:05 +1000

    Remove vestigial comment

--- piecestate.go
+++ piecestate.go
@@ -7,7 +7,6 @@ import (
 // The current state of a piece.
 type PieceState struct {
 	Priority piecePriority
-	// The piece is available in its entirety.
 	storage.Completion
 	// The piece is being hashed, or is queued for hash.
 	Checking bool

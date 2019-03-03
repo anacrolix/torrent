52b3a4e8166778b27533ca5d31d6dbff15ada1eb
Matt Joiner <anacrolix@gmail.com>
2019-03-03 14:30:02 +1100

    Move multiless to missinggo

--- multiless.go
+++ multiless.go
@@ -1,43 +1,8 @@
 package torrent
 
+import "github.com/anacrolix/missinggo"
+
 type (
-	cmper     func() (same, less bool)
-	multiLess struct {
-		ok   bool
-		less bool
-	}
+	multiLess = missinggo.MultiLess
+	cmper     = missinggo.SameLessFunc
 )
-
-func (me *multiLess) Final() bool {
-	if !me.ok {
-		panic("undetermined")
-	}
-	return me.less
-}
-
-func (me *multiLess) FinalOk() (left, ok bool) {
-	return me.less, me.ok
-}
-
-func (me *multiLess) Next(f cmper) {
-	if me.ok {
-		return
-	}
-	same, less := f()
-	if same {
-		return
-	}
-	me.ok = true
-	me.less = less
-}
-
-func (me *multiLess) StrictNext(same, less bool) {
-	if me.ok {
-		return
-	}
-	me.Next(func() (bool, bool) { return same, less })
-}
-
-func (me *multiLess) NextBool(l, r bool) {
-	me.StrictNext(l == r, l)
-}

f5c6f28985bcad532bf6735ae4d9d49098ce87f4
Matt Joiner <anacrolix@gmail.com>
2020-09-29 16:42:52 +1000

    Rename struct size test

--- struct_test.go
+++ struct_test.go
@@ -5,7 +5,7 @@ import (
 	"unsafe"
 )
 
-func TestPieceSize(t *testing.T) {
+func TestStructSizes(t *testing.T) {
 	t.Log("[]*File", unsafe.Sizeof([]*File(nil)))
 	t.Log("Piece", unsafe.Sizeof(Piece{}))
 	t.Log("map[*peer]struct{}", unsafe.Sizeof(map[*peer]struct{}(nil)))

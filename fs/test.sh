e3cfd3d033b2cdaead052a23f5a434c9c757aa90
Matt Joiner <anacrolix@gmail.com>
2021-02-12 19:21:28 +1100

    Rework fs/test.sh
    Updates to use newer debian file, and operate in working directory. Temporary directory management should be done by the caller (such as in CI maybe).

--- fs/test.sh
+++ fs/test.sh
@@ -1,17 +1,17 @@
-set -ex
-repopath=$(cd $(dirname $0)/..; pwd)
-d=$(mktemp -d)
-pushd "$d"
-mkdir mnt torrents
-GOPPROF=http torrentfs -mountDir=mnt -metainfoDir=torrents &
-trap 'set +e; sudo umount -f mnt; pushd; rm -rv "$d"' EXIT
+set -eux
+repopath="$(cd "$(dirname "$0")/.."; pwd)"
+mkdir -p mnt torrents
+GOPPROF=http go run -v -race "$repopath/cmd/torrentfs" -mountDir=mnt -metainfoDir=torrents &> log &
+trap 'set +e; sudo umount -f mnt' EXIT
+debian_file=debian-10.8.0-amd64-netinst.iso
 pushd torrents
-cp "$repopath"/testdata/debian-9.1.0-amd64-netinst.iso.torrent .
+cp "$repopath/testdata/$debian_file.torrent" .
 echo 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4' > sintel.magnet
 popd
-file=debian-9.1.0-amd64-netinst.iso
+file="$debian_file"
 # file=sintel.mp4
 while [ ! -e "mnt/$file" ]; do sleep 1; done
 pv "mnt/$file" | md5sum
+# expect e221f43f4fdd409250908fc4305727d4
 sudo umount mnt
 wait || echo "wait returned" $?

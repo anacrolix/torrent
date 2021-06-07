125d7f0e905898c84345ee10de1ce775fa0c905f
Matt Joiner <anacrolix@gmail.com>
2021-06-07 12:44:51 +1000

    Create go.yml (#497)
    * Create go.yml
    
    * Go version 1.16
    
    * Add benchmarks
    
    * Match CircleCI test coverage
    
    * Add 15 minute timeout
    
    * Don't log to file
    
    * Download modules in separate step, minimize test runs
    
    * Install godo before running torrentfs end-to-end test
    
    * More fixes
    
    * More fixes for GitHub Actions CI

--- fs/test.sh
+++ fs/test.sh
@@ -1,7 +1,7 @@
 set -eux
 repopath="$(cd "$(dirname "$0")/.."; pwd)"
 mkdir -p mnt torrents
-GOPPROF=http godo -v "$repopath/cmd/torrentfs" -mountDir=mnt -metainfoDir=torrents &> log &
+GOPPROF=http godo -v "$repopath/cmd/torrentfs" -mountDir=mnt -metainfoDir=torrents &
 trap 'set +e; sudo umount -f mnt' EXIT
 debian_file=debian-10.8.0-amd64-netinst.iso
 pushd torrents
@@ -11,7 +11,7 @@ popd
 file="$debian_file"
 # file=sintel.mp4
 while [ ! -e "mnt/$file" ]; do sleep 1; done
-pv "mnt/$file" | md5sum
+pv -f "mnt/$file" | md5sum
 # expect e221f43f4fdd409250908fc4305727d4
 sudo umount mnt
 wait || echo "wait returned" $?

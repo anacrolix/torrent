e2a559e2435a0629164f69142274c9a131afc24b
Zilog8 <zeuscoding@gmail.com>
2021-06-08 01:45:35 -0400

    Allow Storage Backends to do their own Hashing (#518)
    * Allow Storage Backends to do their own Hashing
    
    - Describes an optional interface 'SelfHashing' that a storage backend's type implementing 'PieceImpl' may also implement in order to calculate piece hashsums itself.
    
    - Alters the 'hashPiece' function in the torrent package to look for types implementing 'SelfHashing' . If not implemented, calculate the hash as usual.

--- storage/interface.go
+++ storage/interface.go
@@ -22,8 +22,9 @@ type TorrentImpl interface {
 	Close() error
 }
 
-// Interacts with torrent piece data. Optional interfaces to implement include io.WriterTo, such as
-// when a piece supports a more efficient way to write out incomplete chunks
+// Interacts with torrent piece data. Optional interfaces to implement include:
+//   io.WriterTo, such as when a piece supports a more efficient way to write out incomplete chunks.
+//   SelfHashing, such as when a piece supports a more efficient way to hash its contents.
 type PieceImpl interface {
 	// These interfaces are not as strict as normally required. They can
 	// assume that the parameters are appropriate for the dimensions of the
@@ -42,4 +43,9 @@ type PieceImpl interface {
 type Completion struct {
 	Complete bool
 	Ok       bool
+}
+
+// Allows a storage backend to override hashing (i.e. if it can do it more efficiently than the torrent client can)
+type SelfHashing interface {
+	SelfHash() (metainfo.Hash, error)
 }

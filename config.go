c7a0bfe2fc104eb8e4e6c17fbf9748e726e8ac35
ucwong <ucwong@126.com>
2021-06-23 17:21:47 +0800

    package expect fix

--- config.go
+++ config.go
@@ -10,7 +10,7 @@ import (
 	"github.com/anacrolix/dht/v2/krpc"
 	"github.com/anacrolix/log"
 	"github.com/anacrolix/missinggo/v2"
-	"github.com/anacrolix/missinggo/v2/expect"
+	"github.com/anacrolix/missinggo/expect"
 	"golang.org/x/time/rate"
 
 	"github.com/anacrolix/torrent/iplist"

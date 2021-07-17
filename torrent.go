5eab39001f21d6a50c7684676bbe29ba6cd7d302
Matt Joiner <anacrolix@gmail.com>
2021-07-17 18:52:41 +1000

    Add debug level to piece hash failure log message

--- torrent.go
+++ torrent.go
@@ -1857,7 +1857,9 @@ func (t *Torrent) pieceHashed(piece pieceIndex, passed bool, hashIoErr error) {
 		if passed {
 			pieceHashedCorrect.Add(1)
 		} else {
-			log.Fmsg("piece %d failed hash: %d connections contributed", piece, len(p.dirtiers)).AddValues(t, p).Log(t.logger)
+			log.Fmsg(
+				"piece %d failed hash: %d connections contributed", piece, len(p.dirtiers),
+			).AddValues(t, p).SetLevel(log.Debug).Log(t.logger)
 			pieceHashedNotCorrect.Add(1)
 		}
 	}

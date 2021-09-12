2d70f05a083de47e885ef4b2d22024c29c55424f
YenForYang <YenForYang@users.noreply.github.com>
2021-09-12 18:28:39 -0500

    Update main.go

--- cmd/torrent-metainfo-pprint/main.go
+++ cmd/torrent-metainfo-pprint/main.go
@@ -36,11 +36,12 @@ func processReader(r io.Reader) error {
 		fmt.Printf("%s\n", info.Name)
 		return nil
 	}
+	hibs := metainfo.HashInfoBytes()
 	d := map[string]interface{}{
 		"Name":         info.Name,
 		"NumPieces":    info.NumPieces(),
 		"PieceLength":  info.PieceLength,
-		"InfoHash":     metainfo.HashInfoBytes().HexString(),
+		"InfoHash":     hibs.HexString(),
 		"NumFiles":     len(info.UpvertedFiles()),
 		"TotalLength":  info.TotalLength(),
 		"Announce":     metainfo.Announce,

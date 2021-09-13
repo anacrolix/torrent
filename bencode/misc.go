827954ee7b13446bd67daeb8f4edf71a87f8e4df
YenForYang <YenForYang@users.noreply.github.com>
2021-09-13 15:56:49 -0500

    Cheaper byte to string conversion
    Revamped https://github.com/anacrolix/torrent/pull/587

--- bencode/misc.go
+++ bencode/misc.go
@@ -18,13 +18,5 @@ var unmarshalerType = reflect.TypeOf(func() *Unmarshaler {
 }()).Elem()
 
 func bytesAsString(b []byte) string {
-	if len(b) == 0 {
-		return ""
-	}
-	// See https://github.com/golang/go/issues/40701.
-	var s string
-	hdr := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	hdr.Data = uintptr(unsafe.Pointer(&b[0]))
-	hdr.Len = len(b)
-	return s
+	return *(*string)(unsafe.Pointer(&b))
 }

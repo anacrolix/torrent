b333a65e308a1c3f2288b3e217751b5f14c1f4a0
YenForYang <YenForYang@users.noreply.github.com>
2021-09-14 16:38:43 -0500

    Change `Private` field of `metainfo.Info` to `bool`
    Not sure why a pointer is used here since none of the other fields are pointers -- if "private" doesn't exist it's false as expected. This also saves a `nil` check.

--- metainfo/info.go
+++ metainfo/info.go
@@ -17,10 +17,10 @@ type Info struct {
 	Pieces      []byte `bencode:"pieces"`            // BEP3
 	Name        string `bencode:"name"`              // BEP3
 	Length      int64  `bencode:"length,omitempty"`  // BEP3, mutually exclusive with Files
-	Private     *bool  `bencode:"private,omitempty"` // BEP27
 	// TODO: Document this field.
 	Source string     `bencode:"source,omitempty"`
 	Files  []FileInfo `bencode:"files,omitempty"` // BEP3, mutually exclusive with Length
+	Private bool      `bencode:"private,omitempty"` // BEP27
 }
 
 // The Info.Name field is "advisory". For multi-file torrents it's usually a suggested directory

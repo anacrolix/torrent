be8b7b7d35b2eff9fd4d639b041448afe740bf20
YenForYang <YenForYang@users.noreply.github.com>
2021-09-14 06:54:37 -0500

    Optimize `LoopbackListenHost()` (#615)

--- listen.go
+++ listen.go
@@ -3,7 +3,7 @@ package torrent
 import "strings"
 
 func LoopbackListenHost(network string) string {
-	if strings.Contains(network, "4") {
+	if strings.IndexByte(network, '4') != -1 {
 		return "127.0.0.1"
 	} else {
 		return "::1"

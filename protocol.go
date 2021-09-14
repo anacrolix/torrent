635bad08ab991eebc1c8f78363c7889859a97284
YenForYang <YenForYang@users.noreply.github.com>
2021-09-13 20:37:42 -0500

    Update protocol.go

--- protocol.go
+++ protocol.go
@@ -2,8 +2,9 @@ package torrent
 
 import (
 	pp "github.com/anacrolix/torrent/peer_protocol"
+	"github.com/anacrolix/torrent/types"
 )
 
-func makeCancelMessage(r Request) pp.Message {
+func makeCancelMessage(r types.Request) pp.Message {
 	return pp.MakeCancelMessage(r.Index, r.Begin, r.Length)
 }

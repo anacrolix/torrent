f63bdc7fa5fc4b55965a088c8d6fe2e00dbae7d3
YenForYang <YenForYang@users.noreply.github.com>
2021-09-15 17:26:48 -0500

    Update announce.go

--- tracker/udp/announce.go
+++ tracker/udp/announce.go
@@ -4,30 +4,25 @@ import (
 	"encoding"
 
 	"github.com/anacrolix/dht/v2/krpc"
+	"github.com/anacrolix/torrent/tracker/shared"
 )
 
 // Marshalled as binary by the UDP client, so be careful making changes.
+// See https://www.libtorrent.org/udp_tracker_protocol.html
 type AnnounceRequest struct {
 	InfoHash   [20]byte
 	PeerId     [20]byte
 	Downloaded int64
 	Left       int64 // If less than 0, math.MaxInt64 will be used for HTTP trackers instead.
 	Uploaded   int64
-	// Apparently this is optional. None can be used for announces done at
+	// Apparently this is optional. AnnounceEventNone can be used for announces done at
 	// regular intervals.
-	Event     AnnounceEvent
+	Event     shared.AnnounceEvent
 	IPAddress uint32
 	Key       int32
 	NumWant   int32 // How many peer addresses are desired. -1 for default.
 	Port      uint16
-} // 82 bytes
-
-type AnnounceEvent int32
-
-func (e AnnounceEvent) String() string {
-	// See BEP 3, "event", and https://github.com/anacrolix/torrent/issues/416#issuecomment-751427001.
-	return []string{"", "completed", "started", "stopped"}[e]
-}
+} // 82 bytes + 6 bytes padding
 
 type AnnounceResponsePeers interface {
 	encoding.BinaryUnmarshaler

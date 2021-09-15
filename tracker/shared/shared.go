2bb126313be202479ec967182666f1822f4dad08
YenForYang <YenForYang@users.noreply.github.com>
2021-09-15 17:29:00 -0500

    Update shared.go

--- tracker/shared/shared.go
+++ tracker/shared/shared.go
@@ -2,16 +2,18 @@ package shared
 
 type AnnounceEvent int32
 
-const (
-	AnnounceEventNone AnnounceEvent = iota      //
-	
-	// Local peer just completed the torrent.
-	AnnouncedEventCompleted                     // completed
-	// local peer has just resumed this torrent.
-	AnnounceEventStarted                        // started
-	// Local peer is leaving the swarm.
-	AnnounceEventStopped                        // stopped
-)
-
 // TODO: Incorporate use of stringer for AnnounceEvent:
 // go:generate stringer -type=AnnounceEvent -trimprefix=AnnounceEvent -linecomment
+
+// NOTE: the trailing comments are the strings used in generating the `String()` method.
+// See BEP 3, "event", and https://github.com/anacrolix/torrent/issues/416#issuecomment-751427001.
+const (
+	// Default event, equivalent to unspecified
+	AnnounceEventNone AnnounceEvent = iota //
+	// Local peer just completed the torrent.
+	AnnouncedEventCompleted // completed
+	// local peer has just resumed this torrent.
+	AnnounceEventStarted    // started
+	// Local peer is leaving the swarm.
+	AnnounceEventStopped    // stopped
+)

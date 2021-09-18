c740cde71bfb9d5b570c42dc2659c00b121b7bc8
YenForYang <YenForYang@users.noreply.github.com>
2021-09-17 20:35:21 -0500

    metainfo: Remove reflection dependency for `Piece.Hash()` (#661)
    `copy`  will copy exactly `HashSize` bytes here.

--- metainfo/piece.go
+++ metainfo/piece.go
@@ -1,9 +1,5 @@
 package metainfo
 
-import (
-	"github.com/anacrolix/missinggo/v2"
-)
-
 type Piece struct {
 	Info *Info // Can we embed the fields here instead, or is it something to do with saving memory?
 	i    pieceIndex
@@ -23,7 +19,7 @@ func (p Piece) Offset() int64 {
 }
 
 func (p Piece) Hash() (ret Hash) {
-	missinggo.CopyExact(&ret, p.Info.Pieces[p.i*HashSize:(p.i+1)*HashSize])
+	copy(ret[:], p.Info.Pieces[p.i*HashSize:(p.i+1)*HashSize])
 	return
 }
 

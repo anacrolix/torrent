2daad6e564706c36c63b3962fc212f776cc2cf23
Matt Joiner <anacrolix@gmail.com>
2021-09-29 10:13:32 +1000

    Update fuzz to use new standard library support

---
+++ bencode/fuzz_test.go
@@ -0,0 +1,26 @@
+//go:build go1.18
+
+package bencode
+
+import (
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+)
+
+func Fuzz(f *testing.F) {
+	f.Fuzz(func(t *testing.T, b []byte) {
+		c := qt.New(t)
+		var d interface{}
+		err := Unmarshal(b, &d)
+		if err != nil {
+			t.Skip()
+		}
+		b0, err := Marshal(d)
+		c.Assert(err, qt.IsNil)
+		var d0 interface{}
+		err = Unmarshal(b0, &d0)
+		c.Assert(err, qt.IsNil)
+		c.Assert(d0, qt.DeepEquals, d)
+	})
+}
---
+++ metainfo/fuzz_test.go
@@ -0,0 +1,46 @@
+//go:build go1.18
+
+package metainfo
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/anacrolix/torrent/bencode"
+)
+
+func Fuzz(f *testing.F) {
+	// Is there an OS-agnostic version of Glob?
+	matches, err := filepath.Glob(filepath.FromSlash("testdata/*.torrent"))
+	if err != nil {
+		f.Fatal(err)
+	}
+	for _, m := range matches {
+		b, err := os.ReadFile(m)
+		if err != nil {
+			f.Fatal(err)
+		}
+		f.Logf("adding %q", m)
+		f.Add(b)
+	}
+	f.Fuzz(func(t *testing.T, b []byte) {
+		var mi MetaInfo
+		err := bencode.Unmarshal(b, &mi)
+		if err != nil {
+			t.Skip(err)
+		}
+		_, err = bencode.Marshal(mi)
+		if err != nil {
+			panic(err)
+		}
+		info, err := mi.UnmarshalInfo()
+		if err != nil {
+			t.Skip(err)
+		}
+		_, err = bencode.Marshal(info)
+		if err != nil {
+			panic(err)
+		}
+	})
+}

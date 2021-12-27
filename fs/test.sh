e8bd16257c40a5b53ac19c4f8b7669eb41b699e2
Matt Joiner <anacrolix@gmail.com>
2021-12-27 20:27:25 +1100

    fs/test.sh: Use go run instead of godo

--- fs/test.sh
+++ fs/test.sh
@@ -1,12 +1,12 @@
 set -eux
 repopath="$(cd "$(dirname "$0")/.."; pwd)"
 mkdir -p mnt torrents
-GOPPROF=http godo -v "$repopath/cmd/torrentfs" -mountDir=mnt -metainfoDir=torrents &
+GOPPROF=http go run -v "$repopath/cmd/torrentfs" -mountDir=mnt -metainfoDir=torrents &
 trap 'set +e; sudo umount -f mnt' EXIT
 debian_file=debian-10.8.0-amd64-netinst.iso
 pushd torrents
 cp "$repopath/testdata/$debian_file.torrent" .
-godo -v -race "$repopath/cmd/torrent" metainfo "$repopath/testdata/sintel.torrent" magnet > sintel.magnet
+go run -v -race "$repopath/cmd/torrent" metainfo "$repopath/testdata/sintel.torrent" magnet > sintel.magnet
 popd
 file="$debian_file"
 # file=sintel.mp4

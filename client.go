162cef5d1b40a2c0282f0c967796c82477a7d803
Matt Joiner <anacrolix@gmail.com>
2022-01-27 15:12:26 +1100

    When banning an IP drop peers that match

--- client.go
+++ client.go
@@ -1489,6 +1489,14 @@ func (cl *Client) banPeerIP(ip net.IP) {
 		cl.badPeerIPs = make(map[string]struct{})
 	}
 	cl.badPeerIPs[ip.String()] = struct{}{}
+	for _, t := range cl.torrents {
+		t.iterPeers(func(p *Peer) {
+			if p.remoteIp().Equal(ip) {
+				t.logger.Levelf(log.Warning, "dropping peer %v with banned ip %v", p, ip)
+				p.drop()
+			}
+		})
+	}
 }
 
 func (cl *Client) newConnection(nc net.Conn, outgoing bool, remoteAddr PeerRemoteAddr, network, connString string) (c *PeerConn) {

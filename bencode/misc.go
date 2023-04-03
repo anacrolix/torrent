e13fd755ee30acf326335f582317aa53ea3f34f7
Matt Joiner <anacrolix@gmail.com>
2023-04-03 15:10:38 +1000

    bencode: Only use unsafe.String for go>=1.20

--- bencode/misc.go
+++ bencode/misc.go
@@ -2,7 +2,6 @@ package bencode
 
 import (
 	"reflect"
-	"unsafe"
 )
 
 // Wow Go is retarded.
@@ -10,7 +9,3 @@ var (
 	marshalerType   = reflect.TypeOf((*Marshaler)(nil)).Elem()
 	unmarshalerType = reflect.TypeOf((*Unmarshaler)(nil)).Elem()
 )
-
-func bytesAsString(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}

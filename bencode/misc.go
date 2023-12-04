2c2b56344c0735ab6d714c76cc33e85809b4ef9c
Vassili Tchersky <vt+git@vbc.su>
2023-12-04 15:26:18 +0100

    bencode: drop support for go 1.20

--- bencode/misc.go
+++ bencode/misc.go
@@ -2,6 +2,7 @@ package bencode
 
 import (
 	"reflect"
+	"unsafe"
 )
 
 // Wow Go is retarded.
@@ -9,3 +10,7 @@ var (
 	marshalerType   = reflect.TypeOf((*Marshaler)(nil)).Elem()
 	unmarshalerType = reflect.TypeOf((*Unmarshaler)(nil)).Elem()
 )
+
+func bytesAsString(b []byte) string {
+	return unsafe.String(unsafe.SliceData(b), len(b))
+}
--- bencode/string.go
+++
@@ -1,9 +0,0 @@
-//go:build !go1.20
-
-package bencode
-
-import "unsafe"
-
-func bytesAsString(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}

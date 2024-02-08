8ca25738b176b6c12a41c3465ae6f8a9342a3b11
Matt Joiner <anacrolix@gmail.com>
2024-02-08 19:51:57 +1100

    Remove unused request strategy chunk iter types

--- request-strategy/piece.go
+++ request-strategy/piece.go
@@ -1,11 +1,5 @@
 package requestStrategy
 
-type ChunksIterFunc func(func(ChunkIndex))
-
-type ChunksIter interface {
-	Iter(func(ci ChunkIndex))
-}
-
 type Piece interface {
 	Request() bool
 	NumPendingChunks() int

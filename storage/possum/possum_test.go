ee84f96971d4ba933b0c4d8d31f751ff577b3d86
Matt Joiner <anacrolix@gmail.com>
2024-02-22 14:49:03 +1100

    Add possum storage

---
+++ storage/possum/possum_test.go
@@ -0,0 +1,30 @@
+package possumTorrentStorage
+
+import (
+	g "github.com/anacrolix/generics"
+	"github.com/anacrolix/log"
+	possum "github.com/anacrolix/possum/go"
+	possumResource "github.com/anacrolix/possum/go/resource"
+	"github.com/anacrolix/torrent/storage"
+	test_storage "github.com/anacrolix/torrent/storage/test"
+	"testing"
+)
+
+// This should be made to mirror the benchmarks for sqlite storage.
+func BenchmarkProvider(b *testing.B) {
+	possumDir, err := possum.Open(b.TempDir())
+	if err != nil {
+		b.Fatal(err)
+	}
+	possumDir.SetInstanceLimits(possum.Limits{
+		DisableHolePunching: false,
+		MaxValueLengthSum:   g.Some[uint64](test_storage.DefaultPieceSize * test_storage.DefaultNumPieces / 2),
+	})
+	defer possumDir.Close()
+	possumProvider := possumResource.Provider{Handle: possumDir}
+	possumTorrentProvider := Provider{Provider: possumProvider, Logger: log.Default}
+	clientStorageImpl := storage.NewResourcePiecesOpts(
+		possumTorrentProvider,
+		storage.ResourcePiecesOpts{LeaveIncompleteChunks: true})
+	test_storage.BenchmarkPieceMarkComplete(b, clientStorageImpl, test_storage.DefaultPieceSize, test_storage.DefaultNumPieces, 0)
+}

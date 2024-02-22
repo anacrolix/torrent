ad16c7e34d81d19152439644583d02beba2109ff
Matt Joiner <anacrolix@gmail.com>
2024-02-22 17:27:18 +1100

    Add test case from issue #906

---
+++ internal/cmd/issue-906/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/anacrolix/envpprof"
+	"github.com/anacrolix/torrent"
+)
+
+func main() {
+	cfg := torrent.NewDefaultClientConfig()
+	cfg.Debug = true
+	cfg.DisablePEX = true
+	cfg.DisableTrackers = true
+	cfg.NoDHT = true
+	cfg.NoDefaultPortForwarding = true
+
+	c, _ := torrent.NewClient(cfg)
+	defer c.Close()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		c.WriteStatus(w)
+	})
+	torrentFile := os.Args[1]
+	peerAddr := os.Args[2]
+	t, _ := c.AddTorrentFromFile(torrentFile)
+	println(t.AddPeers([]torrent.PeerInfo{{Addr: addr(peerAddr)}}))
+	<-t.GotInfo()
+	t.DownloadAll()
+	c.WaitAll()
+	log.Print("ermahgerd, torrent downloaded")
+}
+
+type addr string
+
+func (a addr) String() string {
+	return string(a)
+}

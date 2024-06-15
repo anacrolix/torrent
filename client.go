60e858fb5be10a3919c2e520673d44d6af250d87
Mark Holt <mark@distributed.vision>
2024-06-15 09:12:34 +0100

    remove wc printer

--- client.go
+++ client.go
@@ -16,7 +16,6 @@ import (
 	"net/netip"
 	"sort"
 	"strconv"
-	"sync/atomic"
 	"time"
 
 	"github.com/cespare/xxhash"
@@ -460,8 +459,6 @@ func (cl *Client) ipIsBlocked(ip net.IP) bool {
 	return blocked
 }
 
-var wcct atomic.Int32
-
 func (cl *Client) wantConns(lock bool) bool {
 	alwaysWantConns := func() bool {
 		if lock {
@@ -474,10 +471,7 @@ func (cl *Client) wantConns(lock bool) bool {
 	if alwaysWantConns() {
 		return true
 	}
-	count := wcct.Add(1)
-	fmt.Println("WC", count)
-	defer fmt.Println("WC", count, "DONE")
-	defer wcct.Add(-1)
+
 	for _, t := range cl.torrentsAsSlice() {
 		if t.wantIncomingConns(true) {
 			return true

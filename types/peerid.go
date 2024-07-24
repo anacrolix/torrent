f63b38aad66251492066ef8c66f6c8abf81540bc
Matt Joiner <anacrolix@gmail.com>
2024-07-24 15:21:02 +1000

    Fix spello

--- types/peerid.go
+++ types/peerid.go
@@ -3,7 +3,7 @@ package types
 // Peer client ID.
 type PeerID [20]byte
 
-// // Pretty prints the ID as hex, except parts that adher to the PeerInfo ID
+// // Pretty prints the ID as hex, except parts that adhere to the PeerInfo ID
 // // Conventions of BEP 20.
 // func (me PeerID) String() string {
 // 	// if me[0] == '-' && me[7] == '-' {

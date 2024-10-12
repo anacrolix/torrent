b104e3bd7ee406b0f3475cff7155921e0285851d
Dmytro Vovk <vovk.dimon@gmail.com>
2024-10-12 13:12:41 +0100

    Revert "fixed discover for new peers connection"

--- torrent.go
+++ torrent.go
@@ -2533,10 +2533,6 @@ func (t *Torrent) wantOutgoingConns(lock bool) bool {
 	conns := t.peerConnsAsSlice(lock)
 	defer conns.free()
 
-	if len(conns) < t.maxEstablishedConns {
-		return true
-	}
-
 	numOutgoingConns := conns.numOutgoingConns()
 	numIncomingConns := len(conns) - numOutgoingConns
 

7db746fd40e71cfca39404f813645eb8b6499559
Matt Joiner <anacrolix@gmail.com>
2025-08-15 11:10:28 +1000

    Add TODO on SEEK_DATA

--- storage/sys_unix.go
+++ storage/sys_unix.go
@@ -12,6 +12,7 @@ import (
 
 func seekData(f *os.File, offset int64) (ret int64, err error) {
 	ret, err = unix.Seek(int(f.Fd()), offset, unix.SEEK_DATA)
+	// TODO: Handle filesystems that don't support sparse files.
 	if err == unix.ENXIO {
 		// File has no more data. Treat as short write like io.CopyN.
 		err = io.EOF

6196e360630c50632f25aa94a9892a5461090731
Matt Joiner <anacrolix@gmail.com>
2025-12-06 21:31:05 +1100

    Remove all the unused "classic" file reader sharing

--- storage/file-io.go
+++ storage/file-io.go
@@ -16,10 +16,21 @@ type fileReader interface {
 	io.ReadCloser
 }
 
+// This gets clobbered by a hybrid mmap implementation if mmap is available.
+var defaultFileIo = func() fileIo {
+	return classicFileIo{}
+}
+
 type fileIo interface {
-	openForSharedRead(name string) (sharedFileIf, error)
+
+	openForSharedRead(name string) (sharableReader, error)
 	openForRead(name string) (fileReader, error)
 	openForWrite(name string, size int64) (fileWriter, error)
 	flush(name string, offset, nbytes int64) error
 	rename(from, to string) error
+}
+
+type sharableReader interface {
+	io.ReaderAt
+	io.Closer
 }
